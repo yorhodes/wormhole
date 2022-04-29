f05f97248d4adda2d6f227622e78002595ad5a7b
Evan Gray <battledingo@gmail.com>
2022-04-29 12:56:45 -0400

    sdk/js,bridge_ui: algorand testnet

---
+++ bridge_ui/src/utils/algorand.ts
@@ -0,0 +1,47 @@
+import { TransactionSignerPair } from "@certusone/wormhole-sdk/lib/esm/algorand";
+import { assignGroupID } from "@certusone/wormhole-sdk/node_modules/algosdk"; // to pass instanceof Transaction check, though there may be another way
+import MyAlgoConnect from "@randlabs/myalgo-connect";
+import { Algodv2, waitForConfirmation } from "algosdk";
+import { ALGORAND_WAIT_FOR_CONFIRMATIONS } from "./consts";
+
+export async function signSendAndConfirmAlgorand(
+  algodClient: Algodv2,
+  txs: TransactionSignerPair[]
+) {
+  const myAlgoConnect = new MyAlgoConnect();
+  assignGroupID(txs.map((tx) => tx.tx));
+  const signedTxns: Uint8Array[] = [];
+  const lsigSignedTxns: Uint8Array[] = [];
+  const walletUnsignedTxns: Uint8Array[] = [];
+  // sign all the lsigs
+  for (const lsigTx of txs) {
+    if (lsigTx.signer) {
+      lsigSignedTxns.push(await lsigTx.signer.signTxn(lsigTx.tx));
+    }
+  }
+  // assemble the txs for the wallet to sign
+  for (const walletTx of txs) {
+    if (!walletTx.signer) {
+      walletUnsignedTxns.push(walletTx.tx.toByte());
+    }
+  }
+  const walletSignedTxns = await myAlgoConnect.signTransaction(
+    walletUnsignedTxns
+  );
+  let lsigIdx = 0;
+  let walletIdx = 0;
+  for (const originalTx of txs) {
+    if (originalTx.signer) {
+      signedTxns.push(lsigSignedTxns[lsigIdx++]);
+    } else {
+      signedTxns.push(walletSignedTxns[walletIdx++].blob);
+    }
+  }
+  await algodClient.sendRawTransaction(signedTxns).do();
+  const result = await waitForConfirmation(
+    algodClient,
+    txs[txs.length - 1].tx.txID(),
+    ALGORAND_WAIT_FOR_CONFIRMATIONS
+  );
+  return result;
+}
