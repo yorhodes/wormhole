d3923b1e521c752bb854e5aae81d8c4dc893ea2b
Evan Gray <battledingo@gmail.com>
2022-09-06 23:36:02 +0000

    node: support for heartbeat spying

--- node/cmd/spy/spy.go
+++ node/cmd/spy/spy.go
@@ -253,8 +253,37 @@ func runSpy(cmd *cobra.Command, args []string) {
 	// Inbound signed VAAs
 	signedInC := make(chan *gossipv1.SignedVAAWithQuorum, 50)
 
+	// Heartbeat updates
+	heartbeatC := make(chan *gossipv1.Heartbeat, 50)
+
 	// Guardian set state managed by processor
-	gst := common.NewGuardianSetState()
+	gst := common.NewGuardianSetState(heartbeatC)
+
+	// Bootstrap guardian set, otherwise heartbeats would be skipped
+	// gst.Set(&common.GuardianSet{
+	// 	Index: 2,
+	// 	Keys: []eth_common.Address{
+	// 		eth_common.HexToAddress("0x58CC3AE5C097b213cE3c81979e1B9f9570746AA5"),
+	// 		eth_common.HexToAddress("0xfF6CB952589BDE862c25Ef4392132fb9D4A42157"),
+	// 		eth_common.HexToAddress("0x114De8460193bdf3A2fCf81f86a09765F4762fD1"),
+	// 		eth_common.HexToAddress("0x107A0086b32d7A0977926A205131d8731D39cbEB"),
+	// 		eth_common.HexToAddress("0x8C82B2fd82FaeD2711d59AF0F2499D16e726f6b2"),
+	// 		eth_common.HexToAddress("0x11b39756C042441BE6D8650b69b54EbE715E2343"),
+	// 		eth_common.HexToAddress("0x54Ce5B4D348fb74B958e8966e2ec3dBd4958a7cd"),
+	// 		eth_common.HexToAddress("0x66B9590e1c41e0B226937bf9217D1d67Fd4E91F5"),
+	// 		eth_common.HexToAddress("0x74a3bf913953D695260D88BC1aA25A4eeE363ef0"),
+	// 		eth_common.HexToAddress("0x000aC0076727b35FBea2dAc28fEE5cCB0fEA768e"),
+	// 		eth_common.HexToAddress("0xAF45Ced136b9D9e24903464AE889F5C8a723FC14"),
+	// 		eth_common.HexToAddress("0xf93124b7c738843CBB89E864c862c38cddCccF95"),
+	// 		eth_common.HexToAddress("0xD2CC37A4dc036a8D232b48f62cDD4731412f4890"),
+	// 		eth_common.HexToAddress("0xDA798F6896A3331F64b48c12D1D57Fd9cbe70811"),
+	// 		eth_common.HexToAddress("0x71AA1BE1D36CaFE3867910F99C09e347899C19C3"),
+	// 		eth_common.HexToAddress("0x8192b6E7387CCd768277c17DAb1b7a5027c0b3Cf"),
+	// 		eth_common.HexToAddress("0x178e21ad2E77AE06711549CFBB1f9c7a9d8096e8"),
+	// 		eth_common.HexToAddress("0x5E1487F35515d02A92753504a8D75471b9f49EdB"),
+	// 		eth_common.HexToAddress("0x6FbEBc898F403E4773E95feB15E80C9A99c8348d"),
+	// 	},
+	// })
 
 	// RPC server
 	s := newSpyServer(logger)
@@ -286,6 +315,17 @@ func runSpy(cmd *cobra.Command, args []string) {
 				if err := s.Publish(v.Vaa); err != nil {
 					logger.Error("failed to publish signed VAA", zap.Error(err))
 				}
+			}
+		}
+	}()
+
+	// Ignore heartbeats
+	go func() {
+		for {
+			select {
+			case <-rootCtx.Done():
+				return
+			case <- heartbeatC:
 			}
 		}
 	}()
